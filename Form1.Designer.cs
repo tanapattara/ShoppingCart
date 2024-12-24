58fe494c1d3b10a3cd1f1da145f95c11dd11fe58
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2024-12-24 11:29:50 +0700

    update form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,26 @@
             button1 = new Button();
             tbCoffeeQuantity = new TextBox();
             tbGreenTeaQuantity = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            label9 = new Label();
+            textBox8 = new TextBox();
+            label10 = new Label();
+            textBox9 = new TextBox();
+            label11 = new Label();
+            textBox10 = new TextBox();
             SuspendLayout();
             // 
             // tbCoffeePrice
@@ -75,7 +95,7 @@
             // 
             // tbTotal
             // 
-            tbTotal.Location = new Point(329, 131);
+            tbTotal.Location = new Point(635, 28);
             tbTotal.Name = "tbTotal";
             tbTotal.Size = new Size(125, 27);
             tbTotal.TabIndex = 4;
@@ -83,7 +103,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(254, 134);
+            label1.Location = new Point(560, 31);
             label1.Name = "label1";
             label1.Size = new Size(42, 20);
             label1.TabIndex = 5;
@@ -91,9 +111,9 @@
             // 
             // button1
             // 
-            button1.Location = new Point(486, 22);
+            button1.Location = new Point(460, 28);
             button1.Name = "button1";
-            button1.Size = new Size(94, 136);
+            button1.Size = new Size(94, 353);
             button1.TabIndex = 6;
             button1.Text = "Check out";
             button1.UseVisualStyleBackColor = true;
@@ -113,11 +133,191 @@
             tbGreenTeaQuantity.Size = new Size(125, 27);
             tbGreenTeaQuantity.TabIndex = 8;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(560, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 20);
+            label2.TabIndex = 10;
+            label2.Text = "Cash";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(635, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(560, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Change";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(635, 94);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(560, 130);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 20);
+            label4.TabIndex = 14;
+            label4.Text = "1000";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(635, 127);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 13;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(560, 163);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 20);
+            label5.TabIndex = 16;
+            label5.Text = "500";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(635, 160);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(560, 196);
+            label6.Name = "label6";
+            label6.Size = new Size(33, 20);
+            label6.TabIndex = 18;
+            label6.Text = "100";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(635, 193);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 17;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(560, 229);
+            label7.Name = "label7";
+            label7.Size = new Size(25, 20);
+            label7.TabIndex = 20;
+            label7.Text = "50";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(635, 226);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(560, 262);
+            label8.Name = "label8";
+            label8.Size = new Size(25, 20);
+            label8.TabIndex = 22;
+            label8.Text = "20";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(635, 259);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 21;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(560, 295);
+            label9.Name = "label9";
+            label9.Size = new Size(25, 20);
+            label9.TabIndex = 24;
+            label9.Text = "10";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(635, 292);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 27);
+            textBox8.TabIndex = 23;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(560, 328);
+            label10.Name = "label10";
+            label10.Size = new Size(17, 20);
+            label10.TabIndex = 26;
+            label10.Text = "5";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(635, 325);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(125, 27);
+            textBox9.TabIndex = 25;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(560, 361);
+            label11.Name = "label11";
+            label11.Size = new Size(17, 20);
+            label11.TabIndex = 28;
+            label11.Text = "1";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(635, 358);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(125, 27);
+            textBox10.TabIndex = 27;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(601, 186);
+            ClientSize = new Size(800, 403);
+            Controls.Add(label11);
+            Controls.Add(textBox10);
+            Controls.Add(label10);
+            Controls.Add(textBox9);
+            Controls.Add(label9);
+            Controls.Add(textBox8);
+            Controls.Add(label8);
+            Controls.Add(textBox7);
+            Controls.Add(label7);
+            Controls.Add(textBox6);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
             Controls.Add(tbGreenTeaQuantity);
             Controls.Add(tbCoffeeQuantity);
             Controls.Add(button1);
@@ -144,5 +344,25 @@
         private Button button1;
         private TextBox tbCoffeeQuantity;
         private TextBox tbGreenTeaQuantity;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label6;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label8;
+        private TextBox textBox7;
+        private Label label9;
+        private TextBox textBox8;
+        private Label label10;
+        private TextBox textBox9;
+        private Label label11;
+        private TextBox textBox10;
     }
 }
